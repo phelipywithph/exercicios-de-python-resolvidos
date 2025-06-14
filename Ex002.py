1a6c2e92184b0383be31feba283d2e93b262b7d6
Phelipy Julião <124466082+phelipywithph@users.noreply.github.com>
2025-06-14 20:45:53 -0300

    Dois primeiros exercicios resolvidos
    Commit contendo os dois primeiros exercícios resolvidos

---
+++ Ex002.py
@@ -0,0 +1,3 @@
+#Exercício Python 2: Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas.
+nome = input("Qual o seu nome? ") #Variável para armazenar o nome da pessoa
+print(f"Boas Vindas, {nome}")
